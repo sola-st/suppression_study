f2a428480c988783881eba8bff90df1219bf0a15
huhuimin236@gmail.com <huhuimin@gmail.com>
2023-07-12 17:23:23 +0000

    Change test to compare expected, actual warning

--- tests/checkers/test_GetPylintWarnings.py
+++ tests/checkers/test_GetPylintWarnings.py
@@ -2,25 +2,29 @@ import subprocess
 import os
 
 def test_GetPylintWarning():
-    demo_path = "demo/" # path
-    if not os.path.exists(demo_path):
-        os.makedirs(demo_path)
+    initial_work_dir = os.getcwd()
+    demo_path = "tests/checkers/demo/" # path
     os.chdir(demo_path)
     demo_repo_git_link = "https://github.com/michaelpradel/suppression-test-python-pylint.git"
     subprocess.run("git clone " + demo_repo_git_link, shell=True)
-    os.chdir("../")
+    os.chdir(initial_work_dir)
 
     subprocess.run(["python3", "src/checkers/GetPylintWarnings.py",
         "--repo_dir=" + demo_path + "suppression-test-python-pylint/",
         "--commit_id=a09fcfe",
         "--results_dir=../"])
 
+    with open("tests/checkers/demo/expected_check_results/a09fcfe/a09fcfe_warnings.csv", "r") as f:
+        expected_warnings = f.readlines()
+    f.close()
+
     with open(demo_path + "checker_results/a09fcfe/a09fcfe_warnings.csv", "r") as f:
-        warnings = f.readlines()
+        actual_warnings = f.readlines()
     f.close()
     
-    assert warnings.__len__() == 2 # totally with 4 warnings in commit a09fcfe
-    for warn in warnings:
-        meta = warn.split(",")
-        assert meta.__len__() == 3 # meta: file_path, warning_type, line_number
-        assert meta[0] == "folder1/foo.py"
+    assert actual_warnings.__len__() == expected_warnings.__len__()
+    for actual_warn, expected_warn in zip(actual_warnings, expected_warnings):
+        assert actual_warn == expected_warn
+
+if __name__=="__main__":
+    test_GetPylintWarning()
