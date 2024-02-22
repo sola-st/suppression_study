20b7f7811b7439b9fbfe9806802421c0ee57b21a
Michael Pradel <michael@binaervarianz.de>
2024-02-22 09:40:53 -0800

    Experiment to measure suppression prevalence on sample of 48 Pylint-using projects (unlike in the previous experiment that we used for OOPSLA'24, the sampled projects are now not biased anymore by whether they contain suppressions)

---
+++ src/suppression_study/experiments/CountSuppressionsOnUnbiasedSample.py
@@ -0,0 +1,12 @@
+from os.path import join
+from suppression_study.experiments.CountSuppressionsOnLatestCommit import CountSuppressionsOnLatestCommit
+
+
+class CountSuppressionsOnUnbiasedSample(CountSuppressionsOnLatestCommit):
+    def __init__(self):
+        super().__init__()
+        self.repo_file = join("data", "python_repos_48_using_pylint.txt")
+
+
+if __name__ == "__main__":
+    CountSuppressionsOnUnbiasedSample().run()
